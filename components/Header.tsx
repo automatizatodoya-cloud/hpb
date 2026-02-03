a4284fa1f62ed2157fc5825ee823f74e9e8b9266
automatizatodoya-cloud <automatizatodoya@gmail.com>
2026-02-03 07:39:05 -0300

    cambio logo ruta public y saco  onerror2

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,10 +20,7 @@ const Header: React.FC = () => {
             src="/img/logo.png" 
             alt="Puerto Bandera Logo" 
             className="h-10 md:h-12"
-            //onError={(e) => {
-            //  (e.target as HTMLImageElement).src = "https://picsum.photos/100/100?text=PB";
-            //}}
-          />
+                      />
           <div className={`hidden md:block transition-colors ${isScrolled ? 'text-blue-900' : 'text-white'}`}>
             <h1 className="text-xl font-bold leading-none">PUERTO BANDERA</h1>
             <p className="text-[10px] tracking-widest uppercase">Rosario - Argentina</p>
