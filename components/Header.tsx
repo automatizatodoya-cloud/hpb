65433b9866d846fd898dfca64c4fdea8ada9d71d
automatizatodoya-cloud <automatizatodoya@gmail.com>
2026-02-03 07:29:14 -0300

    cambio logo4

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ const Header: React.FC = () => {
       <div className="container mx-auto px-6 flex justify-between items-center">
         <div className="flex items-center gap-3">
           <img 
-            src="public/hpb/img/logo.png" 
+            src="hpb/public/img/logo.png" 
             alt="Puerto Bandera Logo" 
             className="h-10 md:h-12"
             onError={(e) => {
